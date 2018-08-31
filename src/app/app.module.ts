249dadd6c8eedcaccfaa11a5dc9c0e421243fe0d
passenger6 <passenger6@gmail.com>
2018-08-31 14:30:17 +0200

    update import path for the demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgxAsideModule } from 'ngx-aside';
-
+import { NgxAsideModule } from '../../projects/ngx-aside/src/lib';
 
 @NgModule({
   declarations: [
