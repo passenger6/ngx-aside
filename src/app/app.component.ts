99787ee2a86c623ae456acd7da0ce475515cdc41
faraway <daniel.georg@carmeq.com>
2018-09-25 16:34:27 +0200

    feat: fix aot compilation bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import {
 } from '@angular/core';
 import { NgxAsideComponent } from 'projects/ngx-aside/src/lib/aside.component';
 
+
 @Component({
-  // tslint:disable-next-line:component-selector
   selector: 'na-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgxAsideModule } from '../../dist/ngx-aside';
+import { NgxAsideModule } from '../../projects/ngx-aside/src/lib';
 
 @NgModule({
   declarations: [
@@ -16,4 +16,4 @@ import { NgxAsideModule } from '../../dist/ngx-aside';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
